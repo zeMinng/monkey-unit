ecf0c5ff52f27df2204d9dea95b62724b004ba47
zeMing <2439340964@qq.com>
2026-05-12 10:38:16 +0800

    Initial commit

---
+++ 禅道自动登录.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         禅道密码自动填充登录
+// @namespace    http://tampermonkey.net/
+// @version      0.6.2
+// @description  禅道--懒人专用的密码填充插件
+// @author       zeMing
+// @match        *://*.project.chinachdu.com/*
+// @match        *://*.project.chinachdu.com/user-login*
+// @icon         https://zentao.net/favicon.ico
+// @license      MIT
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// ==/UserScript==
+
+/**
+ *                  前言
+ * 感谢使用，本脚本针对的是禅道的免登录密码自动填充。你作为使用者，应该具备一定的IT知识。
+ * 代码的第7行、第8行代表的是脚本的运行网址。作者使用的为project.chinachdu.com所有的页面
+ *
+ * 你需要做的是：
+ * 将代码的第7行、第8行进行修改，否则无法正常使用该脚本。(重要)！
+ * 至于怎么修改，可百度@match规则。(重要)！
+ *
+ */
+
+/*
+  jQuery CDN
+  //@require https://cdn.staticfile.org/jquery/3.6.4/jquery.min.js
+*/
+
+/*
+  2023-11-08 1.[优化]: 代码重构、优化
+
+  2023-11-03 1.[优化]: 逻辑优化
+
+  2023-04-19 1.[新增]: 默认选中-保持登陆
+
+  2023-04-14 1.[修复]: 只在登陆页面加载脚本
+
+  2023-04-13 1.[新增]: 密码填充简易版
+*/
+
+(function() {
+  'use strict';
+  /* globals jQuery, $, waitForKeyElements */
+
+  // 解构简易化，勿动
+  const { log } = console
+  let userInfo = {
+    name: '',
+    pass: '',
+  }
+  log('已加载自动填充密码脚本！')
+  const userNameDom = document.querySelector('input[type="text"][name="account"]')
+  const userPassDom = document.querySelector('input[type="password"][name="password"]')
+  const keepLoginDom = document.querySelector('input[type="checkbox"][name="keepLogin[]"]') || document.getElementById("keepLoginon")
+  const submitDom = document.getElementById("submit")
+
+  submitDom && submitDom.addEventListener('click', function() {
+    userInfo = {
+      name: userNameDom.value,
+      pass: userPassDom.value,
+    }
+    GM_setValue('userInfoKey', userInfo)
+  })
+
+  if (userNameDom || userPassDom) {
+    const jsonUser = GM_getValue('userInfoKey')
+    const keys = GM_listValues()
+    if (!jsonUser) return
+    let { name: storageName, pass: storagePass } = jsonUser || {}
+    let { name, pass} = userInfo
+    userNameDom.value = name || storageName
+    userPassDom.value = pass || storagePass
+    keepLoginDom.checked = keepLoginDom.checked || true
+    if (name || pass) return
+    submitDom.click()
+  }
+})()
---
+++ 西石油校园网自动登录.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         西安石油大学校园网自动登录
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  西安石油大学--校园网密码填充插件
+// @author       zeMing
+// @match        *://*.10.123.0.253/*
+// @icon         https://xsyu.edu.cn/favicon.ico
+// @license      MIT
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// ==/UserScript==
+
+/**
+ *                  前言
+ * 感谢使用，本脚本针对的是 <西安石油大学> --校园网密码填充插件。
+ * 作为使用者，你应该对自己的账号和密码妥善保管。
+ * 
+ * 承诺：本脚本仅用于个人学习、研究使用，不得用于任何商业用途，否则后果自负。
+ * 
+ */
+
+(function() {
+  'use strict';
+  /* globals jQuery, $, waitForKeyElements */
+
+  // 解构简易化，勿动
+  const { log } = console
+  let userInfo = {
+    name: '',
+    pass: '',
+  }
+  log('已加载自动填充密码脚本！')
+
+  window.onload = () => {
+    initView()
+  }
+
+  const initView = () => {
+    const userNameDom = document.querySelector('input[type="text"][name="DDDDD"]')
+    const userPassDom = document.querySelector('input[type="password"][name="upass"]')
+    const keepLoginDom = document.querySelector('input[type="checkbox"][name="savePassword"]')
+    const submitDom = document.querySelector('input[type="submit"][name="0MKKey"]')
+
+    if (userNameDom || userPassDom) {
+      const jsonUser = GM_getValue('userInfo_GM')
+      const { name: storageName, pass: storagePass } = jsonUser || {}
+      const { name, pass} = userInfo
+      userNameDom.value = storageName ?? name
+      userPassDom.value = storagePass ?? pass
+      keepLoginDom.checked = keepLoginDom.checked || true
+      submitDom.click()
+    }
+
+    submitDom && submitDom.addEventListener('click', function() {
+      userInfo = {
+        name: userNameDom.value,
+        pass: userPassDom.value,
+      }
+      GM_setValue('userInfoKey', userInfo)
+    })
+  }
+})()
